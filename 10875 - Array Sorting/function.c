80374ac1d12cb9e76dadaa8f06b129c5eefcc054
Loyi <loyi.hsu@gmail.com>
2019-06-25 17:00:48 +0800

    Restore missing codes from local backup on the disk

--- 10875 - Array Sorting/function.c
+++ 10875 - Array Sorting/function.c
@@ -1,1 +1,27 @@
 // Problem: http://140.114.86.238/problem/10875/
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+
+void sortArray(int R, int (*array)[COLUMN])
+{
+    int i, j, k;
+    int ptr;
+    
+    for (i = 0; i < R-1; i++)
+    {
+        for (j = 0; j < R-1-i; j++)
+        {
+            for (k = 0; k<COLUMN; k++)
+            {
+                if (array[j][k] > array[j+1][k])
+                {
+                    ptr = array[j][k];
+                    array[j][k] = array[j+1][k];
+                    array[j+1][k] = ptr;
+                }
+            }
+            
+        }
+    }
+}
--- 10901 - Pointer Integer Array/function.c
+++ 10901 - Pointer Integer Array/function.c
@@ -1,1 +1,23 @@
 // Problem: http://140.114.86.238/problem/10901/
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+
+int** malloc_ptr(int array_size)
+{
+    int **a = (int**)malloc(sizeof(int*)*array_size);
+    return a;
+}
+
+void malloc_array(int **array, int array_size)
+{
+    *array = (int*)malloc(sizeof(int)*array_size);
+    return;
+}
+
+void pointer_ptr_to_array(int *array, int **ptr,int N)
+{
+    int i;
+    for (i = 0; i<N; i++) *(ptr+i) = &array[(i+1)*i/2];
+    return;
+}
