1000f5f82034e1f7e3fb0d8164acb24aa8d1de5e
Loyi <loyi.hsu@gmail.com>
2019-06-25 15:28:05 +0800

    Changes implementation

--- 10066 - I2P homework2a/main.c
+++ 10066 - I2P homework2a/main.c
@@ -1,23 +1,14 @@
-// Problem: http://140.114.86.238/status/view_code/1263983
+// Problem: http://140.114.86.238/problem/10066/
 #include <stdio.h>
-
-float reverse(float);
 
 int main(void)
 {
-	float num, snum, avg;
-	scanf("%f", &num);
-	snum = reverse(num);
-	avg = (num + snum) / 2;
-	printf("%.1f", avg);
+    char digits[3];
+    float num=0, rev=0;
+    
+    scanf("%s", digits);
+    num = (float)digits;
+    rev = (float){digits[2], digits[1], digits[0]};
+    
 	return 0;
-}
-
-float reverse(float x)
-{
-	int a, b, c;
-	a = x / 100;
-	b = (x / 10) - (10 * a);
-	c = x - (100 * a) - (10 * b);
-	return 100 * c + 10 * b + a;
 }
