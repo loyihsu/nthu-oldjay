21e064cb68f44c67f08040499390fc53d928b846
Loyi <loyi.hsu@gmail.com>
2019-07-04 22:42:29 +0800

    Minor formatting changes
    Code not AC

--- Intro to Programming (I)/10901 - Pointer Integer Array/function.c
+++ Intro to Programming (I)/10901 - Pointer Integer Array/function.c
@@ -3,20 +3,17 @@
 #include <stdlib.h>
 #include "function.h"
 
-int** malloc_ptr(int array_size)
-{
+int** malloc_ptr(int array_size) {
     int **a = (int**)malloc(sizeof(int*)*array_size);
     return a;
 }
 
-void malloc_array(int **array, int array_size)
-{
+void malloc_array(int **array, int array_size) {
     *array = (int*)malloc(sizeof(int)*array_size);
     return;
 }
 
-void pointer_ptr_to_array(int *array, int **ptr,int N)
-{
+void pointer_ptr_to_array(int *array, int **ptr,int N) {
     int i;
     for (i = 0; i<N; i++) *(ptr+i) = &array[(i+1)*i/2];
     return;
