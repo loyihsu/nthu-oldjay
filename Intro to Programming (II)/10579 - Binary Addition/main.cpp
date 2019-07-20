fa092d75a65f242d9e885b1ff67f1b215a2a1c17
Loyi <loyi.hsu@gmail.com>
2019-07-20 21:58:09 +0800

    OK (10579)

--- Intro to Programming (II)/10579 - Binary Addition/main.cpp
+++ Intro to Programming (II)/10579 - Binary Addition/main.cpp
@@ -3,33 +3,29 @@
 #include <vector>
 #include <algorithm>
 
-int main(void)
-{
+int main(void) {
     std::ios::sync_with_stdio(false);
-    int testcases;
+
+    int testcases, ps, i;
     char temp;
-    int ps, i;
     std::vector<int> set1, set2;
     
     std::cin >> testcases;
     
-    while (testcases--)
-    {
+    while (testcases--) {
         set1.clear();
         set2.clear();
         
         std::cin >> ps;
         i = ps;
-        while (i--)
-        {
+        while (i--) {
             std::cin >> temp;
             set1.push_back(temp - '0');
         }
         
         i = ps;
         
-        while (i--)
-        {
+        while (i--) {
             std::cin >> temp;
             set2.push_back(temp - '0');
         }
@@ -38,14 +34,13 @@ int main(void)
         std::reverse(set2.begin(), set2.end());
         
         std::vector<int> output;
+
         output.reserve(ps);
-        
         std::transform(set1.begin(), set1.end(), set2.begin(), std::inserter(output,output.begin()), std::plus<int>());
         
         int up = 0;
         
-        for (auto &o: output)
-        {
+        for (auto &o: output) {
             o += up;
             up = o / 2;
             o %= 2;
@@ -54,11 +49,10 @@ int main(void)
         std::reverse(output.begin(), output.end());
         
         for (auto o: output)
-        {
             std::cout << o;
-        }
         
         std::cout << "\n";
     }
+    
     return 0;
 }
