90e20320e42c14809b23bcacff5e4e19f6cce72d
Loyi <loyi.hsu@gmail.com>
2019-07-26 16:00:50 +0800

    10972 (OK)

--- Intro to Programming (II)/10972 - Remove unnecessary parentheses/main.c
+++ Intro to Programming (II)/10972 - Remove unnecessary parentheses/main.c
@@ -3,13 +3,10 @@
 #include <stdlib.h>
 
 typedef enum {ID_A, ID_B, ID_C, ID_D, OP_AND, OP_OR} TokenSet;
-char sym[6] = "ABCD&|";
-
-char expr[200];
+char sym[7] = "ABCD&|", expr[200];
 int pos;
 
-typedef struct _Node
-{
+typedef struct _Node {
     TokenSet data;
     struct _Node *left, *right;
 } BTNode;
@@ -17,13 +14,11 @@ typedef struct _Node
 BTNode* EXPR();
 BTNode* FACTOR();
 
-void freeTree(BTNode* tree)
-{
+void freeTree(BTNode* tree) {
     free(tree);
 }
 
-BTNode* makeNode(char val)
-{
+BTNode* makeNode(char val) {
     BTNode *node = (BTNode*)malloc(sizeof(BTNode));
     int i;
     for (i = 0; i < 6; i++)
@@ -34,74 +29,67 @@ BTNode* makeNode(char val)
     return node;
 }
 
-BTNode* FACTOR()
-{
+BTNode* FACTOR() {
     char c;
     BTNode *node = NULL;
     
-    
-    if (pos >= 0)
-    {
+    if (pos >= 0) {
         c = expr[pos--];
-        if (c >= 'A' && c <= 'D')
+        if (c >= 'A' && c <= 'D') {
             node = makeNode(c);
-        else if (c == ')')
-        {
+        } else if (c == ')') {
             node = EXPR();
-            if (expr[pos--] != '(')
-            {
+            if (expr[pos--] != '(') {
                 printf("Error!");
                 freeTree(node);
             }
         }
     }
+
     return node;
 }
 
-BTNode* EXPR()
-{
+BTNode* EXPR() {
     char c;
     BTNode *node = NULL, *right = NULL;
     
-    if (pos >= 0)
-    {
+    if (pos >= 0) {
         right = FACTOR();
         
-        if (pos > 0)
-        {
+        if (pos > 0) {
             c = expr[pos];
-            if (c == '&' || c == '|')
-            {
+            if (c == '&' || c == '|') {
                 node = makeNode(c);
                 node->right = right;
                 pos--;
                 node->left = EXPR();
-            } else node = right;
-        } else node = right;
+            } else {
+                node = right;
+            }
+        } else {
+            node = right;
+        }
     }
     return node;
 }
 
-void printWithoutPare(BTNode *root)
-{
-    if (root != NULL)
-    {
+void printWithoutPare(BTNode *root) {
+    if (root != NULL) {
         printWithoutPare(root->left);
         printf("%c", sym[root->data]);
-        if (root->right != NULL)
-        {
-            if (root->right->data == OP_OR || root->right->data == OP_AND)
-            {
+        if (root->right != NULL) {
+            if (root->right->data == OP_OR || root->right->data == OP_AND) {
                 printf("(");
                 printWithoutPare(root->right);
                 printf(")");
-            } else printWithoutPare(root->right);
+            } else { 
+                printWithoutPare(root->right);
+            }
         }
     }
 }
 
-int main(void)
-{
+int main(void) {
     scanf("%s", expr);
     pos = strlen(expr) - 1;
     BTNode *node = EXPR();
