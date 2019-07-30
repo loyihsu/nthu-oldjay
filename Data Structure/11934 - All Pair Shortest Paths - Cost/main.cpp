ecaee905452018610264ea94bf4b84f8d377ab71
Loyi <loyi.hsu@gmail.com>
2019-07-30 17:51:39 +0800

    11934 (OK)

--- Data Structure/11934 - All Pair Shortest Paths - Cost/main.cpp
+++ Data Structure/11934 - All Pair Shortest Paths - Cost/main.cpp
@@ -5,48 +5,35 @@ int **dist;
 
 void shortestPath(int n);
 
-int main()
-{
+int main() {
     int noOfMatrix, noOfVertices, i, j;
 
     std::cin >> noOfMatrix;
 
-    while (noOfMatrix--)
-    {
+    while (noOfMatrix--) {
         std::cin >> noOfVertices;
 
         dist = new int*[noOfVertices];
 
         for (i = 0; i < noOfVertices; i++)
-        {
             dist[i] = new int [noOfVertices];
-        }
 
         for (i = 0; i < noOfVertices; i++)
-        {
             for (j = 0; j < noOfVertices; j++)
-            {
                 std::cin >> dist[i][j];
-            }
-        }
 
         shortestPath(noOfVertices);
 
         for (i = 0; i < noOfVertices; i++)
-        {
             for (j = 0; j < noOfVertices; j++)
-            {
                 if (i != j && dist[i][j] != 0)
                     std::cout << "Cost(" << i << "," << j << "):" << dist[i][j] << std::endl;
-            }
-        }
+
         if (noOfMatrix)
             std::cout << std::endl;
 
         for (i = 0; i < noOfVertices; i++)
-        {
             delete[] dist[i];
-        }
 
         delete [] dist;
     }
@@ -54,27 +41,19 @@ int main()
     return 0;
 }
 
-void shortestPath(int n)
-{
+void shortestPath(int n) {
     int source, distination, i;
 
-    for (i = 0; i < n; i++)
-    {
-        for (source = 0; source < n; source++)
-        {
-            for (distination = 0; distination < n; distination++)
-            {
-                if (dist[source][i] && dist[i][distination] && dist[source][distination])
-                {
+    for (i = 0; i < n; i++) {
+        for (source = 0; source < n; source++) {
+            for (distination = 0; distination < n; distination++) {
+                if (dist[source][i] && dist[i][distination] && dist[source][distination]) {
                     if (dist[source][i] + dist[i][distination] < dist[source][distination])
                         dist[source][distination] = dist[source][i] + dist[i][distination];
-                } else if (dist[source][i] && dist[i][distination] && !dist[source][distination])
-                {
+                } else if (dist[source][i] && dist[i][distination] && !dist[source][distination]) {
                     dist[source][distination] = dist[source][i] + dist[i][distination];
                 }
             }
         }
     }
-
-    return;
 }
